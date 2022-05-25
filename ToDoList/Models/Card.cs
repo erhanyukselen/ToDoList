c54413a1adfafab6b938ffc96d8440f2cd386a90
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-05-25 20:59:39 +0300

    Card Add

---
+++ ToDoList/Models/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class Card
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Id { get; set; }
+        public int Size { get; set; }
+        public Card(string title, string content, int id, int size)
+        {
+            Title = title;
+            Content = content;
+            Id = id;
+            Size = size;
+        }
+    }
+}
