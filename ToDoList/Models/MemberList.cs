efc3bd491717f6b9fdcee5421ae39722f02d6cac
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-05-26 21:08:31 +0300

    MemberList Add

---
+++ ToDoList/Models/MemberList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class MemberList
+    {
+        public List<Member> all = new List<Member>();
+
+        public MemberList()
+        {
+            all.Add(new Member(1, "Erhan Ali Yükselen"));
+            all.Add(new Member(2, "Ayberk Kalyoncu"));
+            all.Add(new Member(3, "Faruk Erol"));
+            all.Add(new Member(4, "Fikri Sınar"));
+        }
+
+    }
+}
